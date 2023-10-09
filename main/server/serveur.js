477686d9d67d40c977e38b4a256ae58b0ef6c4e2
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-10-09 21:50:06 +0100

    khalil first commit

---
+++ main/server/serveur.js
@@ -0,0 +1,10 @@
+const express = require ("express") 
+const { createServer } = require('node:http');
+const databaseConnection = require ("./database/index.js")
+const app = express()
+const server = createServer(app);
+const port = 3000 
+
+server.listen(3000, () => {
+    console.log(`server running at ${port}`)
+  });
