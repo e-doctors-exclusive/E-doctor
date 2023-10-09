df92dcace6f57644a5fc27797e64772aac182351
Jihed <zeldaspirit1@gmail.com>
2023-10-09 23:30:27 +0100

    more tables

--- main/server/database/index.js
+++ main/server/database/index.js
@@ -7,6 +7,7 @@ const RoomModel = require("./model/room.js")
 const AppointmentModel = require("./model/appointment.js")
 const MessageModel = require("./model/message.js")
 const ServiceModel = require("./model/services.js")
+const ChatModel = require("./model/chat.js")
 const connection = new Sequelize("edoctor", "root", "root", {
   host: "localhost",
   dialect: "mysql",
@@ -26,8 +27,58 @@ const Room = RoomModel(connection)
 const Appointment = AppointmentModel(connection)
 const Message =MessageModel(connection)
 const Service =ServiceModel(connection)
+const Chat = ChatModel(connection)
 
+// A User can have many Reviews, Appointments, and Messages
+User.hasMany(Review);
+User.hasMany(Appointment);
+User.hasMany(Message);
 
-connection.sync({force:true}).then(() => console.log("Database & tables created!"))
+// A Doctor can have many Reviews, Appointments, Rooms, Messages, and Services
+Doctor.hasMany(Review);
+Doctor.hasMany(Appointment);
+Doctor.hasMany(Room);
+Doctor.hasMany(Message);
+Doctor.hasMany(Service);
 
-module.exports = { User, Doctor, Review, Room, Appointment,Message,Service };
+// A Review belongs to a User and a Doctor
+Review.belongsTo(User);
+Review.belongsTo(Doctor);
+
+// An Appointment belongs to a User, a Doctor, and a Room
+Appointment.belongsTo(User);
+Appointment.belongsTo(Doctor);
+Appointment.belongsTo(Room);
+
+// A Room belongs to a Doctor and has many Appointments
+Room.belongsTo(Doctor);
+Room.hasMany(Appointment);
+
+// A Message belongs to a User (as sender) and a Doctor (as receiver)
+Message.belongsTo(User);
+Message.belongsTo(Doctor);
+
+// A Service belongs to a Doctor
+Service.belongsTo(Doctor);
+
+// A User (as a doctor) can have many Chats
+Doctor.hasMany(Chat);
+
+// A User (as a patient) can have many Chats
+User.hasMany(Chat);
+
+// A Chat belongs to a User (as a doctor)
+Chat.belongsTo(Doctor);
+
+// A Chat belongs to a User (as a patient)
+Chat.belongsTo(User);
+
+// A Chat can have many Messages
+Chat.hasMany(Message);
+
+// A Message belongs to a Chat
+Message.belongsTo(Chat);
+
+// connection.sync({force:true}).then(() => console.log("Database & tables created!"))
+
+module.exports = { User, Doctor, Review, Room, Appointment,Message,Service,Chat };
