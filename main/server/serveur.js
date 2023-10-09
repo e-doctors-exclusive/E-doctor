96fc8a1e3c0770542a790521ca3270609233d2c4
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-10-09 22:00:58 +0100

    done

--- main/server/serveur.js
+++ main/server/serveur.js
@@ -2,6 +2,7 @@ const express = require ("express")
 const { createServer } = require('node:http');
 const databaseConnection = require ("./database/index.js")
 const app = express()
+app.use(express.json())
 const server = createServer(app);
 const port = 3000 
 
