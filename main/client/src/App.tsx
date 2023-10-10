7bbb1305bfb1774ab91fcc0f0ab78ff8d003fc5e
yonkodhia <yasyasdhia@gmail.com>
2023-10-10 18:07:59 +0100

    ffs

--- main/client/src/App.tsx
+++ main/client/src/App.tsx
@@ -6,10 +6,10 @@ import ContactUs from './components/contactUs/ContactUs';
 import DoctorAccount from './components/doctorAccount/DoctorAccount';
 import HomePage from './components/homePage/HomePage';
 
+import  SignUp from './components/doctorAuth/SignUp';
 
 import Service from './components/service/Service';
 import UserSign from './components/userAuth/UserSign';
-import { SignUp } from './components/doctorAuth/SignUp';
 // import Navbar from './components/navBar/NavBar'
 // import Footer from './components/footer/Footer';
 
