acf71fb11ff77f47bfda2915f7413cf4efe386e0
Jihed <zeldaspirit1@gmail.com>
2023-10-10 17:31:19 +0100

    first commit

--- main/client/src/App.tsx
+++ main/client/src/App.tsx
@@ -6,17 +6,19 @@ import ContactUs from './components/contactUs/ContactUs';
 import DoctorAccount from './components/doctorAccount/DoctorAccount';
 import HomePage from './components/homePage/HomePage';
 import Service from './components/service/Service';
-// import Navbar from './components/navBar/NavBar'
+import Navbar from './components/navBar/NavBar'
+import SignUp from './components/userAuth/SignUp';
 // import Footer from './components/footer/Footer';
 function App() {
   return (
     <>  
       {/* <Navbar/> */}
       <Routes>  
-      {/* <Route path="/navbar" element={<Navbar/>}/> */}
+      <Route path="/navbar" element={<Navbar/>}/>
       <Route path="/useraccount" element={<UserAccount/>}/>    
       <Route path="/contactus" element={<ContactUs/>}/>    
-      <Route path="/doctor" element={<DoctorAccount/>}/>    
+      <Route path="/doctor" element={<DoctorAccount/>}/>  
+      <Route path="/doctorSign" element={<SignUp/>} />
       <Route path="/homePage" element={<HomePage/>}/>   
       <Route path="/service" element={<Service/>}/>   
 
