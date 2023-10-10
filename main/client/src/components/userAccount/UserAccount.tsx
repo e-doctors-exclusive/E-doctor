42665589212ded9c12f60d54b62722926a606221
rezsirine <sirinerezgui585@gmail>
2023-10-10 18:44:49 +0100

    ef

--- main/client/src/components/userAccount/UserAccount.tsx
+++ main/client/src/components/userAccount/UserAccount.tsx
@@ -180,6 +180,16 @@ const UserAccount = () => {
               </svg>
             </button>
           </div>
+          {/* <div className="search-user-input">
+    <input type="text" className="search__input" placeholder="Type your text"/>
+    <button className="search__button">
+        <svg className="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+        </svg>
+    </button>
+</div> */}
           <div className="chat_bell_user">
             <svg
               className="chat-user"
@@ -228,7 +238,7 @@ const UserAccount = () => {
           <div className="container_face_user">
             <span className="face_user_visit">Visits for Today</span>
             <span className="face_user_number">104</span>
-            <div className="face_user_container_percent">
+             <div className="face_user_container_percent">
               <div className="pointment-percent">
                 <span className="new_pointment">New Pointment</span>
                 <span className="pointment_number">40</span>
@@ -237,7 +247,7 @@ const UserAccount = () => {
                 <span className="new_pointment">Old Pointment</span>
                 <span className="pointment_number">64</span>
               </div>
-            </div>
+            </div> 
           </div>
         </div>
       </div>
