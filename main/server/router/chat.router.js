4897285331ca3467a3134014bc4b8091d670150c
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-10-10 09:32:06 +0100

    done  by kkhalil

---
+++ main/server/router/chat.router.js
@@ -0,0 +1,13 @@
+const express = require ("express")
+
+const chatRoomRouter = express.Router()
+
+const {getChatRoom,addChatRoom,deleteChatRoom,updateChatRoom} = require("../controller/chat.controller.js")
+
+
+chatRoomRouter.get("/ChatRoom/getChatRoom",getChatRoom)
+chatRoomRouter.post("/ChatRoom/addChatRoom",addChatRoom) 
+chatRoomRouter.delete("/ChatRoom/deleteChatRoom/:id",deleteChatRoom)
+chatRoomRouter.put("/ChatRoom/updateChatRoom",updateChatRoom)
+
+module.exports = chatRoomRouter
