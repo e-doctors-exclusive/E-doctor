aebf8f30979d7fd1ddede8326adc01d5d60c2c60
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-10-10 15:19:07 +0100

    modification

--- main/client/src/App.tsx
+++ main/client/src/App.tsx
@@ -5,20 +5,23 @@ import UserAccount from './components/userAccount/UserAccount'
 import ContactUs from './components/contactUs/ContactUs';
 import DoctorAccount from './components/doctorAccount/DoctorAccount';
 import HomePage from './components/homePage/HomePage';
-import Navbar from './components/navBar/NavBar'
-import Footer from './components/footer/Footer';
+import Service from './components/service/Service';
+// import Navbar from './components/navBar/NavBar'
+// import Footer from './components/footer/Footer';
 function App() {
   return (
     <>  
-      <Navbar/>
+      {/* <Navbar/> */}
       <Routes>  
-      <Route path="/navbar" element={<Navbar/>}/>
+      {/* <Route path="/navbar" element={<Navbar/>}/> */}
       <Route path="/useraccount" element={<UserAccount/>}/>    
       <Route path="/contactus" element={<ContactUs/>}/>    
       <Route path="/doctor" element={<DoctorAccount/>}/>    
       <Route path="/homePage" element={<HomePage/>}/>   
+      <Route path="/service" element={<Service/>}/>   
+
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
       </>
 
   );
