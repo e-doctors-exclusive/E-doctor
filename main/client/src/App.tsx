c65c56ae43846c7983e630c59d47f32288945dea
Jihed <zeldaspirit1@gmail.com>
2023-10-10 18:16:38 +0100

    ffs

--- main/client/src/App.tsx
+++ main/client/src/App.tsx
@@ -5,6 +5,9 @@ import UserAccount from './components/userAccount/UserAccount'
 import ContactUs from './components/contactUs/ContactUs';
 import DoctorAccount from './components/doctorAccount/DoctorAccount';
 import HomePage from './components/homePage/HomePage';
+
+
+
 import Service from './components/service/Service';
 import UserSign from './components/userAuth/UserSign';
 import { SignUp } from './components/doctorAuth/SignUp';
@@ -15,16 +18,17 @@ function App() {
   return (
     <>
       {/* <Navbar/> */}
-      <Routes>  
-      {/* <Route path="/navbar" element={<Navbar/>}/> */}
-      <Route path="/useraccount" element={<UserAccount/>}/>    
-      <Route path="/contactus" element={<ContactUs/>}/>    
-      <Route path="/doctor" element={<DoctorAccount/>}/>  
-      <Route path="/docAuth" element={<SignUp/>}/>   
-      <Route path="/UserSign" element={<UserSign/>} />
-      <Route path="/homePage" element={<HomePage/>}/>   
-      <Route path="/service" element={<Service/>}/>   
-
+      <Routes>
+        {/* <Route path="/navbar" element={<Navbar/>}/> */}
+        <Route path="/useraccount" element={<UserAccount />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/doctor" element={<DoctorAccount />} />
+        <Route path="/docAuth" element={<SignUp />} />
+        <Route path="/UserSign" element={<UserSign />} />
+        <Route path="/doctor" element={<DoctorAccount />} />
+        <Route path="/docAuth" element={<SignUp />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/service" element={<Service />} />
       </Routes>
       {/* <Footer/> */}
     </>
