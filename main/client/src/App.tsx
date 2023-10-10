904b97fbf03468c1b966263df1e71cdc73db18b9
rezsirine <sirinerezgui585@gmail>
2023-10-10 11:41:27 +0100

    routes done

--- main/client/src/App.tsx
+++ main/client/src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
-
+import {  Routes, Route } from "react-router-dom";
+import UserAccount from './components/userAccount/UserAccount'
+import ContactUs from './components/contactUs/ContactUs';
+import DoctorAccount from './components/doctorAccount/DoctorAccount';
+import HomePage from './components/homePage/HomePage';
 function App() {
   return (
-   <div>here</div>
+    <Routes>  
+      <Route path="/useraccount" element={<UserAccount/>}/>    
+      <Route path="/contactus" element={<ContactUs/>}/>    
+      <Route path="/doctor" element={<DoctorAccount/>}/>    
+      <Route path="/homePage" element={<HomePage/>}/>    
+        
+       
+      </Routes>
+    
   );
 }
 
