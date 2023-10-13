bd803ad5e4f95c9cb700a14980065056993f2421
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-10-13 17:32:27 +0100

    khalil

--- main/client/src/components/service/Service.tsx
+++ main/client/src/components/service/Service.tsx
@@ -1,18 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../navBar/NavBar";
 import "./service.css";
 import image from "../../assets/Rectangle 1548.png";
 import MemberCard from "../membersCards/MemberCard";
 import "../membersCards/style.css";
 import userIcon from "../../assets/userIcon.png";
-import Footer from "../footer/Footer"
+import Footer from "../footer/Footer";
 import ServicesCard from "../servicesCards/ServicesCard";
 import { useLocation } from "react-router-dom";
-type props ={
-  setIsLoggedIn:(value: boolean) => void;
+import { RootState,AppDispatch } from '../../redux';
+import { useDispatch,useSelector } from 'react-redux';
+import { addDoctor,fetchDoctors,UpdateDoctor,DeleteDocotr} from "../../redux/doctorSlice";
+type props = {
+  setIsLoggedIn: (value: boolean) => void;
 };
-const Service:React.FC<props> = ({setIsLoggedIn}) => {
-  const location = useLocation()
+enum MedicalInfotype{
+  "Neurosurgeons",
+  "Thoracic Surgeons",
+  "Orthopedic Surgeons",
+  "Plastic Surgeons,",
+  "Oral and Maxillofacial Surgeons",
+  "Family Physicians",
+  "Internists",
+  "Emergency Physicians",
+  "Psychiatrists",
+  "Obstetricians",
+  "Dentists",
+}
+interface objtype{
+  name:string
+  lastName:string
+  Address:string
+  email:string
+  password:string
+  MedicalInfo:MedicalInfotype
+  rating:string
+  avatar:string
+  bio:string
+
+}
+interface UserData {
+  id: number;
+  Address: string;
+  name: string;
+  email: string;
+  avatar: string;
+  lastName: string;
+  createdAt: string;
+  updatedAt: string;
+  password: string;
+}
+const getLocalStorage = (key: string): string | null => {
+  return localStorage.getItem(key);
+};
+const Service: React.FC<props> = ({ setIsLoggedIn }) => {
+  const location = useLocation();
   const fullstar = (
     <i className="fa-solid fa-star fa-sm" style={{ color: "#F3CD03" }}></i>
   );
@@ -25,6 +67,34 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
       style={{ color: "#F3CD03" }}
     ></i>
   );
+
+  const dispatch:AppDispatch = useDispatch()
+  const DoctorData = useSelector((state:RootState)=>state.doctor.data)
+  useEffect(()=>{
+    dispatch(fetchDoctors())
+  },[dispatch])
+// console.log("jijji",DoctorData);
+
+  const handleScrollDown = () => {
+    window.scrollTo({
+      top: window.innerHeight, // Scroll to the height of the viewport
+      behavior: 'smooth', // Add smooth scrolling behavior
+    });
+  }
+
+  const [showDoc,setShowDoc] = useState(false)
+  const [department,setDepartment]=useState<any> ("")
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const myObjectString = getLocalStorage("user");
+    if (myObjectString) {
+      const myObject = JSON.parse(myObjectString);
+      setUserData(myObject);
+      // console.log(myObject)
+    }
+  }, []);
+  // console.log("khalil is good ",userData);
   return (
     <>
       <NavBar setIsLoggedIn={setIsLoggedIn} />
@@ -50,22 +120,39 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
               <h1>Book Appointment</h1>
             </div>
             <div className="right-inputs">
-              <label>Name * </label>
-              <input type="text" placeholder="Name * " />
-              <label> Email adress * </label>
-              <input type="text" placeholder="example@gmail.com" />
-              <label>Departement * </label>
-              <select name="department" id="">
-                <option>Department</option>
-                <option>hello</option>
-                <option>hello</option>
+              <label>Name </label>
+              <input
+                type="text"
+                placeholder="Name  "
+                value={userData?.name ?? ""}
+              />
+              <label> Email adress </label>
+              <input
+                type="text"
+                placeholder="example@gmail.com"
+                value={userData?.email ?? ""}
+              />
+              <label>Departement </label>
+              <select name="department" id="" onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>setDepartment(e.target.value)}>
+                <option selected>Department</option>
+                <option value='Neurosurgeons' >Neurosurgeons</option>
+                <option  value='Thoracic Surgeons'>Thoracic Surgeons</option>
+                <option  value='Orthopedic Surgeons'>Orthopedic Surgeons</option>
+                <option  value='Plastic Surgeons,'>Plastic Surgeons,</option>
+                <option  value='Oral and Maxillofacial Surgeons'>Oral and Maxillofacial Surgeons</option>
+                <option  value='Family Physicians'>Family Physicians</option>
+                <option  value='Internists'>Internists</option>
+                <option  value='Emergency Physicians'>Emergency Physicians</option>
+                <option  value='Psychiatrists'>Psychiatrists</option>
+                <option  value='Obstetricians'>Obstetricians</option>
+                <option  value='Dentists'>Dentists</option>
               </select>
-              <label>Time * </label>
-              <input type="time" value="04:00" />
+              <label>Time</label>
+              <input type="time"/>
             </div>
             <div className="button-right-first-div">
-              <button className="book-appointment-button">
-                Book Appointment
+              <button onClick={()=>(handleScrollDown(),setShowDoc(true))} className="book-appointment-button">
+                Select Doctor{" "}
               </button>
             </div>
           </div>
@@ -73,9 +160,8 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
       </div>
 
       <div className="big-container-after-pict">
-        
         {/* find a doctor div */}
-        
+
         <div className="find-a-doctor">
           <h1>Find A Doctor</h1>
 
@@ -101,6 +187,15 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
           </div>
         </div>
 
+       { showDoc ?  <div  className="doctor-afet-book"  >
+            {DoctorData.map((obj:objtype,i:number)=>{
+              if((obj.MedicalInfo as MedicalInfotype) === (department as MedicalInfotype ))
+              return( 
+           <MemberCard data={obj} />
+            )
+            })}
+        </div>:null}
+
         {/* services we provide  */}
 
         <div className="services-we-provide-container">
@@ -108,7 +203,7 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
             <h1>Services we provide</h1>
           </div>
           <div className="lorem-text">
-            <p style={{fontSize:"16px",color:"black"}}>
+            <p style={{ fontSize: "16px", color: "black" }}>
               Lorem ipsum dolor sit amet consectetur adipiscing elit semper{" "}
               <br />
               dalar elementur tempus hac tellus libero accumsan
@@ -134,7 +229,7 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
             <h1>what our customers say</h1>
           </div>
           <div className="lorem-text">
-            <p style={{ fontSize: "18px" , color:"black" }}>
+            <p style={{ fontSize: "18px", color: "black" }}>
               Problems trying to resolve the conflict between the two major
               realms of <br /> Classical physics: Newtonian mechanics
             </p>
@@ -192,7 +287,7 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
             <h1 style={{ color: "black" }}>FAQ</h1>
           </div>
           <div className="lorem-text">
-            <p style={{ fontSize: "16px" ,color:"black" }}>
+            <p style={{ fontSize: "16px", color: "black" }}>
               Problems trying to resolve the conflict between <br />
               the two major realms of Classical physics: Newtonian mechanics
             </p>
@@ -245,21 +340,18 @@ const Service:React.FC<props> = ({setIsLoggedIn}) => {
             <h5>the quick fox jumps over the lazy dog</h5>
             <h6>Things on a very small scale behave like nothing </h6>
           </div>
-
         </div>
 
-          <div className="subscribe-to">
-              <p className="p-suscribe-service">Subscribe to our newsletter</p>
+        <div className="subscribe-to">
+          <p className="p-suscribe-service">Subscribe to our newsletter</p>
           <div className="search-button">
-            <input type="text" placeholder="Enter your email"/>
+            <input type="text" placeholder="Enter your email" />
             <button className="suscribe-button-service">suscribe</button>
           </div>
-
-          </div>
+        </div>
       </div>
 
-<Footer/>
-
+      <Footer />
     </>
   );
 };
--- main/client/src/redux/index.ts
+++ main/client/src/redux/index.ts
@@ -2,13 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import patient from './patientSlice'
 import doctor from "./doctorSlice"
 import review from "./reviewSlice"
+import appointment from './appointment'
 export const  store = configureStore({
     reducer:{
         review,
         doctor,
-        patient
+        patient,
+        appointment
     }
-
 })
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
